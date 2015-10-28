f2c39791edcd7d8b94d6f8ef2e7f24ebc4d5efd8
Abdul Hannan Ali <abdulhannanali@outlook.com>
2015-10-28 08:40:25 +0000

    implement all rest verbs get, post, patch, put , delete

---
+++ 01/app.js
@@ -0,0 +1,23 @@
+var express = require("express"),
+    mongoose = require("mongoose"),
+    bodyParser = require("body-parser");
+
+var db = mongoose.connect(process.env.IP+"/bookAPI");
+
+var Book = require("./models/bookModel");
+var app = express();
+
+var port = process.env.PORT || "8080";
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+var bookRouter = require("./Routes/bookRoutes")(Book);
+// var authorRouter = require("./Routes/authorRoutes")();
+
+app.use("/api/books", bookRouter);
+// app.use("/api/authors", authorRouter);
+
+app.listen(port, function() {
+    console.log("Gulp is running my app on PORT " + port);
+});
